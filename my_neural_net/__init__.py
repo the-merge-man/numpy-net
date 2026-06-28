d6631b2fbe775ee5d81d4a64f85690596a9270d2
the-merge-man <jacksimonscience@gmail.com>
2026-06-28 04:22:18 -0400

    Update __init__.py

--- my_neural_net/__init__.py
+++ my_neural_net/__init__.py
@@ -1,1 +1,9 @@
+from .model import NumPyNet
+from .data_loader import load_and_preprocess_mnist, load_and_preprocess_cifar10
+
+__all__ = [
+    "NumPyNet",
+    "load_and_preprocess_mnist",
+    "load_and_preprocess_cifar10"
+]
 
